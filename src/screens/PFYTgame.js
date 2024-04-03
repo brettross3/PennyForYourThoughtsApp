9c777d664653e76f7b8ef8754137bd2b817f5d88
robelchane <robel.chane@edu.sait.ca>
2024-04-03 12:47:51 -0600

    Update PFYTgame.js

--- src/screens/PFYTgame.js
+++ src/screens/PFYTgame.js
@@ -55,11 +55,12 @@ const PFYTgame = () => {
             <Text style={styles.letterText}>{letter}</Text>
           </TouchableOpacity>
         ))}
-        <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('SummaryPage')}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('SummaryPage')}
+        >
           <Text style={styles.buttonText}>Go to Summary Page</Text>
         </TouchableOpacity>
-        </View>
       </View>
       <Text style={styles.word}>{word.join(' ')}</Text>
          {accuracy === 1 && (
