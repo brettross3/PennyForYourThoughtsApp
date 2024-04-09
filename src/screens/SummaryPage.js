e8e68981399a7f3452063f0618e97a3a3f00a5cd
GDReadwin <gerald.readwin@edu.sait.ca>
2024-04-08 23:10:34 -0600

    Update PYFT logic and words
    -fixed summary page showing up early issue
    -added visual effect to selected letters
    -modified summary page to add a "game over" version and a "next level" version
    -increased word database

--- src/screens/SummaryPage.js
+++ src/screens/SummaryPage.js
@@ -1,58 +1,55 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-function SummaryPage({ word, correctGuesses, incorrectGuesses, accuracy, startOver }) {
-  return (
+function SummaryPage({ title, word, correctGuesses, incorrectGuesses, accuracy, buttonText, onPress }) {
+ return (
     <View style={styles.container}>
-      <Text style={styles.summary}>Summary</Text>
+      <Text style={styles.summary}>{title}</Text>
       <View style={styles.content}>
         <Text style={styles.text}>Word: {word}</Text>
         <Text style={styles.text}>Correct Guesses: {correctGuesses}</Text>
         <Text style={styles.text}>Incorrect Guesses: {incorrectGuesses}</Text>
-        <Text style={styles.text}>Accuracy: {accuracy.toFixed(2) * 100}%</Text>
-        <TouchableOpacity onPress={startOver} style={styles.button}>
-          <Text style={styles.buttonText}>Try Again</Text>
+        <Text style={styles.text}>Accuracy: {(accuracy * 100).toFixed(2)}%</Text>
+        <TouchableOpacity onPress={onPress} style={styles.button}>
+          <Text style={styles.buttonText}>{buttonText}</Text>
         </TouchableOpacity>
       </View>
     </View>
-  );
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
+ container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 20,
     backgroundColor: '#BCD8C1',
-  },
-  summary: {
+ },
+ summary: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 10,
-  },
-  content: {
+ },
+ content: {
     alignItems: 'center',
-  },
-  text: {
+ },
+ text: {
     fontSize: 20,
     marginBottom: 10,
     color: 'black',
-  },
-  underline: {
-    textDecorationLine: 'underline',
-  },
-  button: {
+ },
+ button: {
     backgroundColor: '#222E50',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
     marginTop: 10,
-  },
-  buttonText: {
+ },
+ buttonText: {
     color: 'white',
     fontSize: 18,
-  },
+ },
 });
 
 export default SummaryPage;
